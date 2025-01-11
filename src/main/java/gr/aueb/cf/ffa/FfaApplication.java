7467b1059b6a7deb53b1f938517fb3f60fece521
Ioannis Kemerlis <ioanniskemerlis@gmail.com>
2025-01-11 19:39:36 +0200

    update

--- src/main/java/gr/aueb/cf/ffa/FfaApplication.java
+++ src/main/java/gr/aueb/cf/ffa/FfaApplication.java
@@ -1,13 +1,9 @@
 package gr.aueb.cf.ffa;
 
 
-import gr.aueb.cf.ffa.model.User;
-import gr.aueb.cf.ffa.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+
 
 @SpringBootApplication
 public class FfaApplication {
